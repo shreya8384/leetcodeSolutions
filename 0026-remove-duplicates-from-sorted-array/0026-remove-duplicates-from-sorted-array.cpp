a9c1f6316c0827312d55009a50cff030515abce3
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2024-01-04 18:10:20 +0530

    Time: 11 ms (48.24%), Space: 18.7 MB (54.47%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+         // return length of j and index+1 is equals to length n we have to return the count of total number of unique elements
+        return i+1;
+        
+    }};
+*/
