b329e9d99652c11bc9f72fcf4e9a969c44576131
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2024-03-05 15:16:47 +0530

    Time: 29 ms (11.16%), Space: 23.6 MB (7.93%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int ans;
+        for(auto x:mp){
+            if(x.second == 1) ans =x.first;
+        }
+        return ans;
+    }
+};
+
+
+
+/*
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int ans;
+        for(auto x:mp){
+            if(x.second == 1) ans= x.first;
+        }      
+        return ans;
+    }
+};
+*/
