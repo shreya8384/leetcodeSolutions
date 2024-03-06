d12b15997518f6626e6faf290ccfce0ab6e8ef17
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2024-03-06 20:50:50 +0530

    Time: 28 ms (12.13%), Space: 23.6 MB (8.12%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp;
+      /*  unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
@@ -10,10 +10,23 @@ public:
             if(x.second == 1) ans =x.first;
         }
         return ans;
+        */
+         int ans;
+    unordered_map<int,int>mp;
+
+    for(int i=0;i<nums.size();i++){
+        mp[nums[i]]++;
+    }
+
+    for(auto x:mp){
+        if(x.second == 1)
+          ans = x.first;
+    }
+    return ans;
     }
 };
 
-
+// using xor also we can slove this in strivers A2Z sheet
 
 /*
 class Solution {
