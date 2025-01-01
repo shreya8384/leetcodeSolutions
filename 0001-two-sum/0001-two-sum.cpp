c79a96e6ece7c41abaadbd9b84ae96f08295bac3
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-01-01 18:29:29 +0530

    Time: 3 ms (76.91%), Space: 14.9 MB (16.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -14,6 +14,7 @@ public:
        }
        return ans;
         */
+        /*
         vector<int>ans;
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
@@ -24,6 +25,19 @@ public:
             }
             mpp[nums[i]] = i;
         }   
+        return ans;
+        */
+        //new code
+        vector<int>ans;
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.find(target-nums[i])!=mpp.end()){
+                ans.push_back(mpp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            mpp[nums[i]] = i;
+        }
         return ans;
     }
 };
