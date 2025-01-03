8dd25ff45d994a5814d415e951ce84f87ab81b9c
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-01-03 23:51:45 +0530

    Time: 0 ms (100%), Space: 71.7 MB (35.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        // int max_till_now = nums[0];
+        // int n = nums.size();
+        // int curr = 0;
+        // for(int i=0;i<n;i++){
+        //     curr = curr+nums[i];
+        //     max_till_now = max(max_till_now,curr);
+        //     if(curr<0) curr=0;
+        // }
+        // return max_till_now;
         int max_till_now = nums[0];
         int n = nums.size();
         int curr = 0;
