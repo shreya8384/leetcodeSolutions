ae7fc8070e69b1da14e09cdfbe4e2dffafcb4644
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-01-07 22:26:54 +0530

    Time: 0 ms (100%), Space: 20.8 MB (25.63%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -7,18 +7,16 @@ public:
         // }
         // int ans;
         // for(auto x:mp){
-        //     if(x.second == 1) ans =x.first;
+        //     if(x.second == 1) ans = x.first;
         // }
         // return ans;
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        int ans;
-        for(auto x:mp){
-            if(x.second == 1) ans = x.first;
-        }
-        return ans;
+
+    int ans = 0;
+    for(int i=0 ; i<nums.size();i++){
+    ans = ans^nums[i];
+     }
+   return ans;
+
     }
 };
 
