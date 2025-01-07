1bdd3791dd4fadf11663852ea246f9be96c4b8de
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-01-07 22:21:33 +0530

    Time: 16 ms (9.99%), Space: 23.9 MB (13.94%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-      /*  unordered_map<int,int>mp;
+        // unordered_map<int,int>mp;
+        // for(int i=0;i<nums.size();i++){
+        //     mp[nums[i]]++;
+        // }
+        // int ans;
+        // for(auto x:mp){
+        //     if(x.second == 1) ans =x.first;
+        // }
+        // return ans;
+        unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
         int ans;
         for(auto x:mp){
-            if(x.second == 1) ans =x.first;
+            if(x.second == 1) ans = x.first;
         }
         return ans;
-        */
-         int ans;
-    unordered_map<int,int>mp;
-
-    for(int i=0;i<nums.size();i++){
-        mp[nums[i]]++;
-    }
-
-    for(auto x:mp){
-        if(x.second == 1)
-          ans = x.first;
-    }
-    return ans;
     }
 };
 
 // using xor also we can slove this in strivers A2Z sheet
-
+/*
+int findUnique(int *arr, int size)
+{
+   int ans = 0;
+   for(int i=0 ; i<size;i++){
+       ans = ans^arr[i];
+   }
+   return ans;
+}
+*/
 /*
 class Solution {
 public:
