7b75abee3b075bfc3effe51d26a67d4a4aeec1de
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-01-11 21:02:20 +0530

    Time: 0 ms (100%), Space: 29.5 MB (62.64%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,3 +1,28 @@
+//#include <bits/stdc++.h>
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    //     int n = nums.size();
+    //     k = k % n; // Ensure k is within the range [0, n)
+
+    //     // Reverse the entire array
+    //     reverse(nums.begin(), nums.end());
+        
+    //     // Reverse the first k elements
+    //     reverse(nums.begin(), nums.begin() + k);
+        
+    //     // Reverse the rest of the elements after k
+    //     reverse(nums.begin() + k, nums.end());
+    // }
+           int n = nums.size();
+           k = k % n;
+           reverse(nums.begin(),nums.end());
+           reverse(nums.begin(),nums.begin()+k);
+           reverse(nums.begin()+k,nums.end());
+    }
+};
+
+/*
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
@@ -9,7 +34,7 @@ public:
          nums = temp;
     }
 };
-
+*/
 /*
 class Solution {
 public:
