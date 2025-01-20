649c1d93a13ca41fba9eca32bff2ff084b4d3b13
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-01-20 23:42:54 +0530

    Time: 30 ms (92.12%), Space: 18.5 MB (74.45%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -18,13 +18,23 @@ public:
         //     b= b==NULL ? headA: b->next;
         // }
         // return a;
-        if(headA == NULL || headB == NULL) return NULL;
-        ListNode *a = headA;
-        ListNode *b = headB;
-        while(a!=b){
-            a = a == NULL ? headB: a->next;
-            b = b == NULL ? headA: b->next;
-        }
-        return a;
+
+          if (headA == NULL || headB == NULL) return NULL;
+
+    ListNode* temp1 = headA;
+    ListNode* temp2 = headB;
+
+    while (temp1 != temp2) {
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+
+        if (temp1 == temp2) return temp1;
+
+        if (temp1 == NULL) temp1 = headB;
+        if (temp2 == NULL) temp2 = headA;
+    }
+
+    return temp1;
+        
     }
 };
