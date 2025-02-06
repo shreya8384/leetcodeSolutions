c75666c6ac8316420dbd38173078145de10e7d88
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-02-06 16:15:59 +0530

    Time: 7 ms (61.09%), Space: 128.2 MB (94.77%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
@@ -17,12 +18,12 @@ public:
         return ans;
     }
 };
+*/
 
-/*
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-       /*
+       
        vector<int>ans(nums.size(),0);
         int indexpos= 0, indexneg = 1;
         for(auto num:nums){
@@ -36,20 +37,5 @@ public:
             }
         }
         return ans;
-        
-        vector<int>ans(nums.size(),0);
-        int indexpos = 0, indexneg = 1;
-        for(auto num : nums){
-            if(num>0){
-                ans[indexpos] = num;
-                indexpos +=2;
-            }
-            if(num<0){
-                ans[indexneg] = num;
-                indexneg += 2;
-            }
-        }
-        return ans;
     }
 };
-*/
