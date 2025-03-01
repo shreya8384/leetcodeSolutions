77db86ba4512e1ef8cf7607ec2024e0b4fcc63ca
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-03-01 21:55:56 +0530

    Time: 0 ms (100%), Space: 9.5 MB (98.79%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,3 +1,35 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.length()!=t.length()) return false;
+//         int n = s.length();
+//         unordered_map<char,int>mpp;
+//         for(int i=0;i<n;i++){
+//             mpp[s[i]]++;
+//             mpp[t[i]]--;
+            
+//         }
+//         for(auto x:mpp){
+//             if(x.second) return false;
+      
+//         }
+//           return true;
+//     }
+        
+// };
+
+
+/* //Approach 1: Sorting
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
+*/
+/*//Approach 2: Hash Table
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -14,6 +46,33 @@ public:
       
         }
           return true;
-    }   
-    
+    } 
+};
+*/
+
+//Approach 3: Hash Table (Using Array)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+        
+        // Count the frequency of characters in string s
+        for (char x : s) {
+            count[x - 'a']++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 };
