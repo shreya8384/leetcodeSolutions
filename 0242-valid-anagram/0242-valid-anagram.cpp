dae9fa3ced67976358aa2d0db00ffe00f3c4e6ee
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-03-01 21:48:42 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,62 +1,6 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       /* if(s.length() != t.length()) return false;
-        int n = s.length();
-        unordered_map<char,int>mpp;
-        for(int i=0;i<n;i++){
-            mpp[s[i]]++;
-            mpp[t[i]]--;
-        }
-        for(auto x:mpp){
-            if(x.second) return false;
-        }
-        return true;*/
-        /*
-        if(s.length() != t.length())return false;
-        int n = s.length();
-        unordered_map<char,int>mpp;
-        for(int i=0;i<n;i++){
-            mpp[s[i]]++;
-            mpp[t[i]]--;
-            
-        }
-        for(auto x:mpp){
-            if(x.second)return false;
-        }
-        return true;
-        */
-        /*
-        if(s.length() != t.length())return false;
-        int n = s.length();
-        unordered_map<char,int>mpp;
-        for(int i=0;i<n;i++){
-            mpp[s[i]]++;
-            mpp[t[i]]--;
-        }
-        for(auto x:mpp){
-            if(x.second)return false;
-        }
-        return true;
-        */
-        if(s.length()!=t.length())return false;
-        int n = s.length();
-        unordered_map<char,int>mp;
-        for(int i=0;i<n;i++){
-            mp[s[i]]++;
-            mp[t[i]]--;
-        }
-        for(auto x:mp){
-            if(x.second)return false;
-        }
-        return true;
-    }
-};
-
-/*
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
         int n = s.length();
         unordered_map<char,int>mpp;
@@ -70,8 +14,6 @@ public:
       
         }
           return true;
-    }
-       
+    }   
     
 };
-*/
