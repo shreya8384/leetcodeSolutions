ac9436960ec95230bca5c0fd1094528bb788b287
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-03-02 00:39:58 +0530

    Time: 2 ms (78.64%), Space: 14.9 MB (35.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,19 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         /*
-       vector<int>ans;
-       unordered_map<int, int>mpp;
-       for(int i=0;i<nums.size();i++){
-           if(mpp.find(target-nums[i])!=mpp.end()){
-               ans.push_back(mpp[target-nums[i]]);
-               ans.push_back(i);
-               return ans;
-           }
-           mpp[nums[i]] = i;
-       }
-       return ans;
-        */
-        /*
         vector<int>ans;
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
@@ -27,17 +14,18 @@ public:
         }   
         return ans;
         */
-        //new code
+
         vector<int>ans;
         unordered_map<int,int>mpp;
         for(int i=0;i<nums.size();i++){
-            if(mpp.find(target-nums[i])!=mpp.end()){
+            if(mpp.find(target-nums[i]) != mpp.end()){
                 ans.push_back(mpp[target-nums[i]]);
                 ans.push_back(i);
                 return ans;
             }
             mpp[nums[i]] = i;
         }
-        return ans;
+         return ans;
+        
     }
 };
