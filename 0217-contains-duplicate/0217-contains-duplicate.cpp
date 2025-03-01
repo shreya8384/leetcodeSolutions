ab8c0be733b3245ccbc91df6f9e13d5c91df576d
Shreya Verma <57412566+shreya8384@users.noreply.github.com>
2025-03-01 17:38:48 +0530

    Time: 29 ms (68.92%), Space: 73.6 MB (36.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -6,25 +7,44 @@ public:
             mpp[nums[i]]++;
         }
         for(auto x : mpp){
-            if(x.second > 1)return true;
+            if(x.second >= 2)return true;
         }
         return false;
     }
 };
+*/
+/*
+//first solution Time complexity: O(nlogn) Space complexity: O(n) by sorting
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        
+        return false;        
+    }
+};
+*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// second solution- set Time complexity: O(n) Space complexity: O(n)
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>numSet;
+        for(int n:nums){
+            if(numSet.find(n)!= numSet.end()){
+                return true;
+            }
+            numSet.insert(n);
+        }     
+        return false;
+    }
+};
 
 /*
 class Solution {
